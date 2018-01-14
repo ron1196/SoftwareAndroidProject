41c659c271e6c6230ff4d12daac7a7fb2e4457ff
ron1196 <ron1196@gmail.com>
2018-01-14 17:00:15 +0200

    Add files via upload
    Add Python Admin Manager

---
+++ Additional Files/FuelManagerAdmin/main.py
@@ -0,0 +1,69 @@
+import firebase_admin
+from firebase_admin import db
+from firebase_admin import auth
+from firebase_admin import credentials
+
+
+def show_users():
+    ref = db.reference('users')
+    users = ref.get()
+    for x in users:
+        print("\n", "User ID: ", x)
+        for y in users[x]:
+            print("\t", y, ':', users[x][y])
+
+
+def add_user():
+    email = input("Enter Email: ")
+    password = input("Enter Password: ")
+    try:
+        user = auth.create_user(
+            email=email,
+            email_verified=True,
+            password=password
+            #photo_url='https://example.com/photo.png'
+        )
+        users = db.reference('users')
+        users.child(user.uid).set({'address': '', 'budget': 0, 'city': '', 'firstName': '', 'lastName': ''})
+        print("User: ", user.uid, " successfully created")
+    except ValueError:
+        print("Invalid arguments")
+
+
+def delete_user():
+    uid = input("Enter User ID: ")
+    try:
+        auth.delete_user(uid=uid)
+        users = db.reference('users')
+        users.child(uid).delete()
+        print("Successfully deleted user")
+    except firebase_admin.auth.AuthError:
+        print("User not found")
+
+
+cred = credentials.Certificate("fuelmanager-188122-firebase-adminsdk-ssjoq-d64a420c68.json")
+
+firebase_admin.initialize_app(cred, {'databaseURL': 'https://fuelmanager-188122.firebaseio.com/'})
+
+running = True
+while running:
+    print("""
+1.Show Users Database
+2.Add a User
+3.Delete a User
+4.Exit/Quit
+""")
+
+    ans = input("What would you like to do? ")
+    if ans == "1":
+        show_users()
+    elif ans == "2":
+        add_user()
+    elif ans == "3":
+        delete_user()
+    elif ans == "4":
+        running = False
+    else:
+        print("\n Not Valid Choice Try again")
+
+
